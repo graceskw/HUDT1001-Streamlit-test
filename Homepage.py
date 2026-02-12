a9a00a9483fab1037dc56c6973a34536e23e1400
So Ki Wai Grace <so.kwgrace@gmail.com>
2026-02-12 14:44:14 +0800

    testing commit in vs code

--- Homepage.py
+++ Homepage.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+    page_title="HUDT1001",
+    page_icon="🖥️",
 )
 
 st.write("""# Welcome to my website!
